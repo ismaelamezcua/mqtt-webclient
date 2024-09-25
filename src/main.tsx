a79c31fbe5ff321d3690b6540f79fd2a8d39e1e0
Ismael Amezcua <amezcua.ismael@gmail.com>
2024-09-25 09:06:28 -0600

    Feat: create theme for defaultProps

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
-import { MantineProvider } from "@mantine/core";
+import {
+  Button,
+  createTheme,
+  MantineProvider,
+  TimelineItem,
+} from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 
+const theme = createTheme({
+  focusRing: "always",
+  defaultRadius: 0,
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Notifications />
       <App />
     </MantineProvider>
-  </StrictMode>
+  </StrictMode>,
 );
